64de78d25487c2562726fcde9343a8de914ec642
ytyghost <yytytview@gmail.com>
2025-03-28 13:21:03 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'a3ad24b8-dbe8-48a5-a07c-c5b8c5853370';
 
 const พร็อกซีไอพีs = ['104.248.145.216'];
 
